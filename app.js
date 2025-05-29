42adaf3a4ae22ba4813584b11902c8b3fd09a3d3
Nathaniel Itty <nathanielitty@gmail.com>
2025-05-29 13:15:47 -0400

    Simulate broken deployment 2

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`)
-})
-
 app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
 })
@@ -18,4 +14,8 @@ app.get('/health', (req, res) => {
   // eslint-disable-next-line no-constant-condition
   if (true) throw('error...  ')
   res.send('ok')
+})
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`)
 })
